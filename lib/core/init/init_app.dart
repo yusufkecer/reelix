484ef14ea30d61134097a6902409dfe2245b9b5a
yusufkecer <yusufkecer0007@gmail.com>
2025-08-27 22:43:19 +0300

    firebase crashlytics added

--- lib/core/init/init_app.dart
+++ lib/core/init/init_app.dart
@@ -1,4 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:reelix/injection/locator.dart';
@@ -8,6 +10,9 @@ abstract final class InitApp {
   static Future<void> init() async {
     final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
     FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+    await Firebase.initializeApp();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
     await Locator.initializeService();
     await EasyLocalization.ensureInitialized();
   }
